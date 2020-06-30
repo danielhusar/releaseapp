15bfe27658387d072699ddad67b2d52897f92738
Daniel Husar <dano.husar@gmail.com>
2020-06-30 11:50:52 -0700

    Remove unnecessary mock

--- components/__tests__/EnvironmentDetail.test.tsx
+++ components/__tests__/EnvironmentDetail.test.tsx
@@ -2,16 +2,6 @@ import React from 'react'
 import { render, environment } from './helpers'
 import EnvironmentDetail from '../EnvironmentDetail'
 
-const RealDate = Date.now
-
-beforeAll(() => {
-  global.Date.now = jest.fn(() => new Date('2020-04-20T15:48:38.039Z').getTime())
-})
-
-afterAll(() => {
-  global.Date.now = RealDate
-})
-
 describe('EnvironmentDetail', () => {
   it('renders', () => {
     const { container } = render(<EnvironmentDetail {...environment} />)
