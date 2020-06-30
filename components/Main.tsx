bf7a9c3ae0a3f6e515f68e056ad24fd0f9981ccc
Daniel Husar <dano.husar@gmail.com>
2020-06-30 14:12:30 -0700

    Make main bigger

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,7 +3,7 @@ import styled from './Styled'
 const Main = styled.main`
   display: grid;
   grid-template-columns: [left-gutter] 1fr [content] 12fr [right-gutter] 1fr;
-  max-width: 960px;
+  max-width: 1000px;
   margin: 0 auto;
 `
 
